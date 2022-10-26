3675f9badf667f0195493cb690c497290f4e5ffa
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-10-26 16:08:00 +0700

    variable arr = 20, variable n,variable i

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -8,6 +8,17 @@ namespace SearchAlgorithm
 {
     class Program
     {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the Array
+        int n;
+        //Get the number of elements to store in the Array
+        int i;
+
+        public void input()
+        {
+
+        }
         static void Main(string[] args)
         {
         }
